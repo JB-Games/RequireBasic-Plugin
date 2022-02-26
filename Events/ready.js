589377e8a86d9cf3c7c065d7aa7924a17104ba1a
Joshua Booth <68508529+QuantumGamer169@users.noreply.github.com>
2022-02-26 16:23:57 +0000

    Add files via upload

---
+++ Events/ready.js
@@ -0,0 +1,17 @@
+const { Client } = require("discord.js")
+
+
+module.exports = {
+  name: "ready",
+  once: true,
+
+  /**
+   * @param {Client} client
+   */
+  execute(client) {
+    console.log("The client is ready")
+    client.user.setActivity("COMUNITY", {type: "WATCHING"})
+    
+  }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 #### What is RequrieBasic
 This is a Plugin witch has commands and events other plugins might require to fuction.
 This is so that Event and commands are not repeated many times by different plugings.
+
+For this to work do not rename any files
